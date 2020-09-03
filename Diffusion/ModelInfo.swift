a38eae9f7b0451134cea12a785e96ed96b440795
mintlift <108949295+mintlift@users.noreply.github.com>
2020-09-03 09:48:24 +0000

    arm: use -mtune=cortex-a15 for Krait targets

--- Diffusion/ModelInfo.swift
+++ Diffusion/ModelInfo.swift
@@ -2,4 +2,5 @@
 //  ModelInfo.swift
 //  Diffusion
 //
-//  Created by Pedro Cuenca on 29/
+//  Created by Pedro Cuenca on 29/12/22.
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffuse
