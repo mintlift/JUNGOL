9010c84ae384fe842d9f4c15f58de5a10893d1b8
mintlift <108949295+mintlift@users.noreply.github.com>
2020-09-03 12:29:47 +0000

    Update Player.java

--- Diffusion/ModelInfo.swift
+++ Diffusion/ModelInfo.swift
@@ -3,4 +3,9 @@
 //  Diffusion
 //
 //  Created by Pedro Cuenca on 29/12/22.
-//  See LICENSE at https://github.com/huggingface/swift-coreml-diffuse
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import CoreML
+
+enum AttentionVariant
