5ebdebbee73f58218385577f7d5932c851e62524
mintlift <108949295+mintlift@users.noreply.github.com>
2020-12-13 21:40:02 +0000

    inject

--- Diffusion/State.swift
+++ Diffusion/State.swift
@@ -3,4 +3,9 @@
 //  Diffusion
 //
 //  Created by Pedro Cuenca on 17/1/23.
-//  See LICENSE at https://github.com/huggin
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import Combine
+import SwiftUI
+i
