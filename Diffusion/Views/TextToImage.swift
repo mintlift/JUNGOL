ad596855b6e8ac8e867ecda282a535171261d0d1
mintlift <108949295+mintlift@users.noreply.github.com>
2021-03-25 14:11:16 +0000

    Fix quoting.

--- Diffusion/Views/TextToImage.swift
+++ Diffusion/Views/TextToImage.swift
@@ -3,4 +3,9 @@
 //  Diffusion
 //
 //  Created by Pedro Cuenca on December 2022.
-//  See LICENSE at https://github.com/huggingface
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import SwiftUI
+import Combine
+import StableDif
