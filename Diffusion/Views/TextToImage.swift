d4e134d1d3954df69a13139ae661a1aced7f1c38
mintlift <108949295+mintlift@users.noreply.github.com>
2021-03-29 10:23:38 +0000

    initial

--- Diffusion/Views/TextToImage.swift
+++ Diffusion/Views/TextToImage.swift
@@ -14,4 +14,7 @@ import StableDiffusion
 /// Presents "Share" + "Save" buttons on Mac; just "Share" on iOS/iPadOS.
 /// This is because I didn't find a way for "Share" to show a Save option when running on macOS.
 struct ShareButtons: View {
-  
+    var image: CGImage
+    var name: String
+    
+    var filen
