7a3230ecdf3b6e4ce60f915a7c44811870652250
mintlift <108949295+mintlift@users.noreply.github.com>
2021-04-09 20:36:06 +0000

    Update ReadMe

--- Diffusion/Views/TextToImage.swift
+++ Diffusion/Views/TextToImage.swift
@@ -34,4 +34,5 @@ struct ShareButtons: View {
                     do {
                         let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent("\(filename).png")
                         try imageData.write(to: fileURL)
-                        let contr
+                        let controller = UIDocumentPickerViewController(forExporting: [fileURL])
+ 
