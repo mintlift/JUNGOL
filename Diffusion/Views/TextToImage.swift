86221ddadd3b3e459f1089cd3e474edb25a6d1b9
mintlift <108949295+mintlift@users.noreply.github.com>
2021-04-20 18:28:56 +0000

    Funceble Test - Autosave

--- Diffusion/Views/TextToImage.swift
+++ Diffusion/Views/TextToImage.swift
@@ -63,4 +63,5 @@ struct ImageWithPlaceholder: View {
                 return AnyView(ProgressView())
             }
             let step = Int(progress.step) + 1
-    
+            let fraction = Double(step) / Double(progress.stepCount)
+     
