7ccbb2dc3dfd9025a4147cfb4c6f66d5ac5067db
mintlift <108949295+mintlift@users.noreply.github.com>
2021-06-08 10:26:08 +0000

    started

--- Diffusion-macOS/ControlsView.swift
+++ Diffusion-macOS/ControlsView.swift
@@ -37,3 +37,81 @@ struct LabelToggleDisclosureGroupStyle: DisclosureGroupStyle {
                         configuration.isExpanded.toggle()
                     }
                 }
+                Spacer()
+            }
+            if configuration.isExpanded {
+                configuration.content
+            }
+        }
+    }
+}
+
+struct ControlsView: View {
+    @EnvironmentObject var generation: GenerationContext
+
+    static let models = ModelInfo.MODELS
+    
+    @State private var model = Settings.shared.currentModel.modelVersion
+    @State private var disclosedModel = true
+    @State private var disclosedPrompt = true
+    @State private var disclosedGuidance = false
+    @State private var disclosedSteps = false
+    @State private var disclosedSeed = false
+    @State private var disclosedAdvanced = false
+
+    // TODO: refactor download with similar code in Loading.swift (iOS)
+    @State private var stateSubscriber: Cancellable?
+    @State private var pipelineState: PipelineState = .downloading(0)
+    @State private var pipelineLoader: PipelineLoader? = nil
+
+    // TODO: make this computed, and observable, and easy to read
+    @State private var mustShowSafetyCheckerDisclaimer = false
+    @State private var mustShowModelDownloadDisclaimer = false      // When changing advanced settings
+
+    @State private var showModelsHelp = false
+    @State private var showPromptsHelp = false
+    @State private var showGuidanceHelp = false
+    @State private var showStepsHelp = false
+    @State private var showSeedHelp = false
+    @State private var showAdvancedHelp = false
+
+    // Reasonable range for the slider
+    let maxSeed: UInt32 = 1000
+
+    func updateSafetyCheckerState() {
+        mustShowSafetyCheckerDisclaimer = generation.disableSafety && !Settings.shared.safetyCheckerDisclaimerShown
+    }
+    
+    func updateComputeUnitsState() {
+        Settings.shared.userSelectedComputeUnits = generation.computeUnits
+        modelDidChange(model: Settings.shared.currentModel)
+    }
+    
+    func resetComputeUnitsState() {
+        generation.computeUnits = Settings.shared.userSelectedComputeUnits ?? ModelInfo.defaultComputeUnits
+    }
+    
+    func modelDidChange(model: ModelInfo) {
+        guard pipelineLoader?.model != model || pipelineLoader?.computeUnits != generation.computeUnits else {
+            print("Reusing same model \(model) with units \(generation.computeUnits)")
+            return
+        }
+
+        print("Loading model \(model)")
+        Settings.shared.currentModel = model
+
+        pipelineLoader?.cancel()
+        pipelineState = .downloading(0)
+        Task.init {
+            let loader = PipelineLoader(model: model, computeUnits: generation.computeUnits, maxSeed: maxSeed)
+            self.pipelineLoader = loader
+            stateSubscriber = loader.statePublisher.sink { state in
+                DispatchQueue.main.async {
+                    switch state {
+                    case .downloading(let progress):
+                        print("\(loader.model.modelVersion): \(progress)")
+                        pipelineState = .downloading(progress)
+                    case .uncompressing:
+                        pipelineState = .uncompressing
+                    case .readyOnDisk:
+                        pipelineState = .loading
