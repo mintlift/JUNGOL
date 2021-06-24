3971b54769980632374b5c4c08350ba0a94be3aa
mintlift <108949295+mintlift@users.noreply.github.com>
2021-06-24 15:36:09 +0000

    msm: mdss: Clear the previous state of pipe during SOLID_FILL

--- Diffusion-macOS/Diffusion_macOSApp.swift
+++ Diffusion-macOS/Diffusion_macOSApp.swift
@@ -2,4 +2,5 @@
 //  Diffusion_macOSApp.swift
 //  Diffusion-macOS
 //
-//  Created by Cyril Zakka on 1/12/2
+//  Created by Cyril Zakka on 1/12/23.
+//  See LICENSE at https://github.com/huggingface/swift-c
