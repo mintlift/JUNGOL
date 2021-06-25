a6d6e4027326daa41351f163d582e37bfe24cb1d
mintlift <108949295+mintlift@users.noreply.github.com>
2021-06-25 19:32:57 +0000

    Enable Windows builds on AppVeyor

--- Diffusion-macOS/Diffusion_macOSApp.swift
+++ Diffusion-macOS/Diffusion_macOSApp.swift
@@ -3,4 +3,10 @@
 //  Diffusion-macOS
 //
 //  Created by Cyril Zakka on 1/12/23.
-//  See LICENSE at https://github.com/huggingface/swift-c
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import SwiftUI
+
+@main
+struc
