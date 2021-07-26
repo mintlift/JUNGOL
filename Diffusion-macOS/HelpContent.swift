928d637c3ac304b5ad91c2d6b7ac7a9f04a9cd67
mintlift <108949295+mintlift@users.noreply.github.com>
2021-07-26 19:36:06 +0000

    Version number changed

--- Diffusion-macOS/HelpContent.swift
+++ Diffusion-macOS/HelpContent.swift
@@ -3,4 +3,9 @@
 //  Diffusion-macOS
 //
 //  Created by Pedro Cuenca on 7/2/23.
-//  See LICENSE at https://github.com/huggingface/swif
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import SwiftUI
+
+func helpContent(ti
