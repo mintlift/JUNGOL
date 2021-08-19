644f95cd15c94c05ad8d488b5744069db62d1cc3
mintlift <108949295+mintlift@users.noreply.github.com>
2021-08-19 13:31:30 +0000

    Loops

--- Diffusion-macOS/HelpContent.swift
+++ Diffusion-macOS/HelpContent.swift
@@ -40,4 +40,4 @@ func helpContent(title: String, description: AttributedString, showing: Binding<
 func modelsHelp(_ showing: Binding<Bool>) -> some View {
     let description = try! AttributedString(markdown:
         """
-        Diffusers launches with a
+        Diffusers launches with a set of 5 models that can be downloaded from the Huggi
