6eb190c42e158fe0eb74b294682d7e1efd7af85a
mintlift <108949295+mintlift@users.noreply.github.com>
2021-09-07 05:46:22 +0000

    Added new logo

--- Diffusion-macOS/HelpContent.swift
+++ Diffusion-macOS/HelpContent.swift
@@ -62,4 +62,5 @@ func modelsHelp(_ showing: Binding<Bool>) -> some View {
         
         This is a special so-called _distilled_ model, half the size of the others. It runs faster and requires less RAM, try it out if you find generation slow!
         
-        """, options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhi
+        """, options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace))
+    return helpContent(title: "Available Models", descript
