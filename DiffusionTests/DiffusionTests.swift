442fc0b784df4c0dc7d2535e751aed69e1166162
mintlift <108949295+mintlift@users.noreply.github.com>
2022-01-17 10:25:13 +0000

    add blog upgrade

--- DiffusionTests/DiffusionTests.swift
+++ DiffusionTests/DiffusionTests.swift
@@ -3,4 +3,9 @@
 //  DiffusionTests
 //
 //  Created by Pedro Cuenca on December 2022.
-//  See LICENSE at https://github.com/huggingface/swift-coreml-diffu
+//  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
+//
+
+import XCTest
+
+final class DiffusionTests
