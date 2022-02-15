b64f8a79bdb32c741abd82ef5185c6fc5f22e816
mintlift <108949295+mintlift@users.noreply.github.com>
2022-02-15 14:45:21 +0000

    New translations Russian from Crowdin [skip ci]

--- DiffusionTests/DiffusionTests.swift
+++ DiffusionTests/DiffusionTests.swift
@@ -16,4 +16,7 @@ final class DiffusionTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-   
+    }
+
+    func testExample() throws {
+        // This is an example 
