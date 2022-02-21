2dfe5d3ed9cee739fca24d6f4dbb8a3d9e8e7a5e
mintlift <108949295+mintlift@users.noreply.github.com>
2022-02-21 16:43:01 +0000

    Added installation instructions to README

--- DiffusionTests/DiffusionTests.swift
+++ DiffusionTests/DiffusionTests.swift
@@ -20,4 +20,5 @@ final class DiffusionTests: XCTestCase {
 
     func testExample() throws {
         // This is an example of a functional test case.
-        // Use XCTAssert and related f
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // A
