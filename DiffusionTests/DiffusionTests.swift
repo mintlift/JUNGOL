85882d4ebc397adce383d1e7872f7d06d69fce02
mintlift <108949295+mintlift@users.noreply.github.com>
2022-02-27 08:59:04 +0000

    incremented ADAMS version

--- DiffusionTests/DiffusionTests.swift
+++ DiffusionTests/DiffusionTests.swift
@@ -21,4 +21,5 @@ final class DiffusionTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and a
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpecte
