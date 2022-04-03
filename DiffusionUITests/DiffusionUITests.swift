f6679a6e1eb02b528f9bbdb55756ec42ce926904
mintlift <108949295+mintlift@users.noreply.github.com>
2022-04-03 21:13:50 +0000

    Fix travis errors

--- DiffusionUITests/DiffusionUITests.swift
+++ DiffusionUITests/DiffusionUITests.swift
@@ -13,4 +13,4 @@ final class DiffusionUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In
+        // In UI tests it is usually best to stop immediately when a failure occurs.
