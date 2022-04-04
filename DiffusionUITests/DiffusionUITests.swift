67148967b9493792e2fa2968addd124d420d49ec
mintlift <108949295+mintlift@users.noreply.github.com>
2022-04-04 13:24:39 +0000

    copy update

--- DiffusionUITests/DiffusionUITests.swift
+++ DiffusionUITests/DiffusionUITests.swift
@@ -14,3 +14,6 @@ final class DiffusionUITests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s 
