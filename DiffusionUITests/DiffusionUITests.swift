16c7e2ccb927650186d239fef408a50cf694b5dd
mintlift <108949295+mintlift@users.noreply.github.com>
2022-04-13 20:33:37 +0000

    Version 0.3.3

--- DiffusionUITests/DiffusionUITests.swift
+++ DiffusionUITests/DiffusionUITests.swift
@@ -16,4 +16,4 @@ final class DiffusionUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s 
+        // In UI tests it’s important to set the initial state - such as interface orienta
