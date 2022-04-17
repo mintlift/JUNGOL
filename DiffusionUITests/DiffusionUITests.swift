afa980dec3d699abd540d128e2aad45d14760377
mintlift <108949295+mintlift@users.noreply.github.com>
2022-04-17 22:43:37 +0000

    chore(package): update mocha to version 6.2.0

--- DiffusionUITests/DiffusionUITests.swift
+++ DiffusionUITests/DiffusionUITests.swift
@@ -16,4 +16,4 @@ final class DiffusionUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orienta
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp met
