e8b6e3de7703eb43dd5a3b039ebf2e8f4c2482ec
mintlift <108949295+mintlift@users.noreply.github.com>
2022-05-15 21:43:24 +0000

    More PEP8

--- DiffusionUITests/DiffusionUITests.swift
+++ DiffusionUITests/DiffusionUITests.swift
@@ -34,4 +34,6 @@ final class DiffusionUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicati
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            
